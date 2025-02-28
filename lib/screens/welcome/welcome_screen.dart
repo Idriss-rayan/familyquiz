f747e221b98bb53caa94fb87245f4c822b1c5376
Idriss-rayan <rayanidriss27@gmail.com>
2025-02-28 22:00:24 +0600

    first commit

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,10 +1,74 @@
+import 'package:familyquiz/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:websafe_svg/websafe_svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          WebsafeSvg.asset(
+            "assets/icons/bg.svg",
+            fit: BoxFit.cover,
+          ),
+          SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Spacer(
+                  flex: 2,
+                ),
+                Text(
+                  "Let's play Quiz,",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Text("Enter your information below"),
+                Spacer(),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xff1c2341),
+                    hintText: "Full Name",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                  ),
+                ),
+                Spacer(),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: Text(
+                      "Let's Start Quiz",
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                            color: Colors.black,
+                          ),
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 2,
+                ),
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
