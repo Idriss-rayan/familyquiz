0e7bb331fa3de75debecdafe4aa5832904b3f9a0
Idriss-rayan <rayanidriss27@gmail.com>
2025-03-04 23:08:06 +0600

    second day of my ramadan

--- lib/screens/quiz/components/option.dart
+++ lib/screens/quiz/components/option.dart
@@ -18,7 +18,7 @@ class option extends StatelessWidget {
       child: Row(
         children: [
           Text(
-            "1. text",
+            "1. Text",
             style: TextStyle(
               color: kGrayColor,
               fontSize: 16,
