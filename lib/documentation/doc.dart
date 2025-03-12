8e95f0e8eaa14b9544bab43f271b31785c3e4307
Idriss-rayan <rayanidriss27@gmail.com>
2025-03-13 00:33:27 +0600

    elevent ramadan

--- lib/documentation/doc.dart
+++ lib/documentation/doc.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:familyquiz/documentation/histoire.dart'; // Assurez-vous que cette classe est correctement importée.
 
 class Doc extends StatelessWidget {
   Doc({super.key});
-
-  final List<Widget> pages = [
-    PageWidget(
-      bgColor: const Color.fromARGB(255, 242, 175, 4),
-      imagePath: "assets/icons/test1.png",
-    ),
-    PageWidget(
-      bgColor: const Color.fromARGB(255, 4, 241, 119),
-      imagePath: "assets/icons/test2.png",
-    ),
-    PageWidget(
-      gradientColors: [Colors.red, Colors.deepOrangeAccent],
-      imagePath: "assets/icons/test3.png",
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,58 +12,199 @@ class Doc extends StatelessWidget {
         pages: pages,
         fullTransitionValue: 300,
         enableLoop: false,
-        enableSideReveal: true,
-        slideIconWidget: const Icon(
+        enableSideReveal: false,
+        slideIconWidget: Icon(
           Icons.arrow_back_ios,
-          size: 24, // Increased for better visibility
-          color: Colors.white,
+          size: 20,
         ),
         waveType: WaveType.liquidReveal,
         positionSlideIcon: 0.5,
       ),
     );
   }
-}
 
-class PageWidget extends StatelessWidget {
-  final Color? bgColor;
-  final List<Color>? gradientColors;
-  final String imagePath;
-
-  const PageWidget({
-    super.key,
-    this.bgColor,
-    this.gradientColors,
-    required this.imagePath,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
+  final pages = [
+    // Première page avec un fond jaune
+    Container(
       width: double.infinity,
-      decoration: BoxDecoration(
-        color: bgColor,
-        gradient: gradientColors != null
-            ? LinearGradient(
-                colors: gradientColors!,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              )
-            : null,
-      ),
+      color: const Color.fromARGB(255, 242, 175, 4),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
             padding: const EdgeInsets.only(bottom: 25),
             child: Image.asset(
-              imagePath,
+              "assets/icons/test1.png",
               height: 300,
               width: 300,
             ),
           ),
         ],
       ),
-    );
-  }
+    ),
+    // Deuxième page avec un fond vert
+    Container(
+      width: double.infinity,
+      color: const Color.fromARGB(255, 4, 241, 119),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25),
+            child: Image.asset(
+              "assets/icons/test2.png",
+              height: 300,
+              width: 300,
+            ),
+          ),
+        ],
+      ),
+    ),
+    // Dernière page avec un fond dégradé et texte
+    Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.white, Colors.grey],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/icons/Ahijo.png",
+                width: 500,
+                height: 200,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  Name.Ahmadou,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors
+                        .black, // Le texte en blanc pour le faire ressortir
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.justify, // Justifie le texte
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    // pour paul biya
+    Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.white, Colors.blue],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), // Bordure arrondie
+                  image: DecorationImage(
+                    image: AssetImage("assets/icons/biya.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                height: 220,
+                width: 300,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  Name.Biya,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors
+                        .black, // Le texte en blanc pour le faire ressortir
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.justify, // Justifie le texte
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+
+    // pour ernest
+
+    Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 210, 147, 1),
+            const Color.fromARGB(255, 104, 82, 1)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), // Bordure arrondie
+                  image: DecorationImage(
+                    image: AssetImage("assets/icons/ernest.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                height: 220,
+                width: 300,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  Name.ernest,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors
+                        .black, // Le texte en blanc pour le faire ressortir
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.justify, // Justifie le texte
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ];
 }
---
+++ lib/documentation/histoire.dart
@@ -0,0 +1,27 @@
+class Name {
+  static const String Ahmadou = """
+Ahmadou Ahidjo est né le 24 août 1924 à Garoua, dans le nord du Cameroun. Issu d’une famille peule, 
+il suit une éducation coranique et française avant d’entrer dans l’administration coloniale. Son intelligence 
+et son charisme lui permettent d’accéder rapidement aux sphères du pouvoir. 
+En 1947, il est élu député à l’Assemblée territoriale sous administration française. Progressivement, il devient 
+une figure clé des négociations pour l’indépendance du Cameroun. En 1957, il est nommé vice-président du gouvernement 
+autonome du Cameroun, puis devient Premier ministre en 1958. 
+Le 1er janvier 1960, le Cameroun obtient son indépendance et Ahmadou Ahidjo est élu premier président de la République. 
+Son gouvernement met en place un système politique centralisé avec un parti unique, l’UNC (Union nationale camerounaise). 
+Son régime est marqué par une forte répression contre les opposants, notamment les militants de l’UPC, un mouvement indépendantiste.
+""";
+
+  static const String Biya = """
+Paul Biya, né le 13 février 1933 à Mvomeka’a, est une figure emblématique de la vie politique camerounaise. Après des études brillantes en droit et en sciences politiques en France, il rentre au Cameroun et occupe plusieurs postes stratégiques au sein de l’administration. Son parcours remarquable le mène à la fonction de Premier ministre en 1975 sous la présidence d’Ahmadou Ahidjo.  
+Le 6 novembre 1982, après la démission du président Ahidjo, Paul Biya accède à la magistrature suprême et devient président de la République du Cameroun. Dès le début de son mandat, il engage des réformes visant à moderniser l’administration et à renforcer la stabilité du pays.  
+Dans les années 1990, sous son leadership, le Cameroun adopte le multipartisme, marquant une évolution politique majeure. Grâce à sa vision, il œuvre pour la paix, l’unité nationale et le développement socio-économique du pays. Il est régulièrement réélu par le peuple camerounais, témoignant de la confiance placée en lui pour conduire la nation vers le progrès.  
+Son engagement pour la stabilité et la souveraineté du Cameroun demeure constant, et il continue à jouer un rôle clé dans la consolidation du développement du pays. Son leadership expérimenté fait de lui l’un des chefs d’État les plus respectés sur le continent africain.
+""";
+
+  static const String ernest = """
+Ernest Ouandié est une figure emblématique de la lutte pour l’indépendance du Cameroun. Né en 1924, il devient instituteur avant de s’engager activement dans la politique en rejoignant l’Union des Populations du Cameroun (UPC), un mouvement indépendantiste fondé par Ruben Um Nyobé.
+Après l’assassinat de ce dernier en 1958, Ouandié devient l’un des principaux leaders du mouvement et poursuit la lutte contre l’administration coloniale française. Lorsque le Cameroun obtient son indépendance en 1960 sous la présidence d’Ahmadou Ahidjo, l’UPC continue son combat, dénonçant la politique du nouveau gouvernement.
+Ernest Ouandié prend la tête de la branche armée du parti et mène une résistance dans l’Ouest du pays. Traqué pendant plusieurs années, il est finalement arrêté en 1970. Après un procès, il est condamné à mort et exécuté publiquement à Bafoussam le 15 janvier 1971.
+Aujourd’hui, il est reconnu comme un héros de la lutte pour l’indépendance du Cameroun. Son engagement et son sacrifice restent une source d’inspiration pour de nombreux Camerounais attachés aux valeurs de souveraineté et de justice sociale.
+""";
+}
