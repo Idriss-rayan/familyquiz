da70b4a5aafe6b7209014514a9ef2cd49b87681b
Idriss-rayan <rayanidriss27@gmail.com>
2025-03-21 17:40:35 +0600

    solve problem of the UI

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:familyquiz/auth/auth_service.dart';
+import 'package:familyquiz/pages/login_page.dart';
 import 'package:familyquiz/pages/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -15,15 +16,27 @@ class _RegisterPageState extends State<RegisterPage> {
   //text controllers
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
 
-  // login button pressed
-  void login() async {
+  // signUp button pressed
+  void signUp() async {
     final email = _emailController.text;
     final password = _passwordController.text;
+    final confirmPassword = _confirmPasswordController.text;
 
-    //attempt login ...
+    // check that password match
+    if (password != confirmPassword) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("passwords don't match")));
+      return;
+    }
+
+    //attempt signUp ...
     try {
-      await authService.signInWithEmailPassword(email, password);
+      await authService.signUpWithEmailPassword(email, password);
+
+      // pop this register pages
+      Navigator.pop(context);
     }
 
     // catch any errors..
@@ -115,63 +128,90 @@ class _RegisterPageState extends State<RegisterPage> {
                 child: Column(
                   children: <Widget>[
                     FadeInUp(
-                        duration: Duration(milliseconds: 1800),
-                        child: Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                  color: Color.fromRGBO(143, 148, 251, 1)),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Color.fromRGBO(143, 148, 251, .2),
-                                    blurRadius: 20.0,
-                                    offset: Offset(0, 10))
-                              ]),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                    border: Border(
-                                        bottom: BorderSide(
-                                            color: Color.fromRGBO(
-                                                143, 148, 251, 1)))),
-                                child: TextField(
-                                  controller: _emailController,
-                                  decoration: InputDecoration(
-                                      labelText: "Email or Phone number",
-                                      labelStyle: TextStyle(
-                                        color:
-                                            Color.fromARGB(250, 142, 124, 247),
-                                      ),
-                                      border: InputBorder.none,
-                                      //hintText: ,
-                                      hintStyle:
-                                          TextStyle(color: Colors.grey[700])),
+                      duration: Duration(milliseconds: 1800),
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                                color: Color.fromRGBO(143, 148, 251, 1)),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, .2),
+                                  blurRadius: 20.0,
+                                  offset: Offset(0, 10))
+                            ]),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Color.fromRGBO(
+                                              143, 148, 251, 1)))),
+                              child: TextField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                    labelText: "Email or Phone number",
+                                    labelStyle: TextStyle(
+                                      color: Color.fromARGB(250, 142, 124, 247),
+                                    ),
+                                    border: InputBorder.none,
+                                    //hintText: ,
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[700])),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: _passwordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    labelText: "Confirm Password",
+                                    labelStyle: TextStyle(
+                                      color: const Color.fromARGB(
+                                          250, 142, 124, 247),
+                                    ),
+                                    border: InputBorder.none,
+                                    //hintText: "Password",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[700])),
+                              ),
+                            ),
+                            Container(
+                              //padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                      color: Color.fromRGBO(143, 148, 251,
+                                          1)), // Ajout de la ligne
                                 ),
                               ),
-                              Container(
-                                padding: EdgeInsets.all(8.0),
-                                child: TextField(
-                                  controller: _passwordController,
-                                  obscureText: true,
-                                  decoration: InputDecoration(
-                                      labelText: "Password",
-                                      labelStyle: TextStyle(
-                                        color: const Color.fromARGB(
-                                            250, 142, 124, 247),
-                                      ),
-                                      border: InputBorder.none,
-                                      //hintText: "Password",
-                                      hintStyle:
-                                          TextStyle(color: Colors.grey[700])),
-                                ),
-                              )
-                            ],
-                          ),
-                        )),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: _passwordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    labelText: "Password",
+                                    labelStyle: TextStyle(
+                                      color: const Color.fromARGB(
+                                          250, 142, 124, 247),
+                                    ),
+                                    border: InputBorder.none,
+                                    //hintText: "Password",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[700])),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                     SizedBox(
                       height: 30,
                     ),
@@ -179,7 +219,7 @@ class _RegisterPageState extends State<RegisterPage> {
                         duration: Duration(milliseconds: 1900),
                         child: InkWell(
                           splashColor: const Color.fromARGB(255, 172, 172, 222),
-                          onTap: login,
+                          onTap: signUp,
                           child: Container(
                             height: 50,
                             decoration: BoxDecoration(
@@ -190,7 +230,7 @@ class _RegisterPageState extends State<RegisterPage> {
                                 ])),
                             child: Center(
                               child: Text(
-                                "Login",
+                                "Sign UP",
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontWeight: FontWeight.bold),
@@ -204,9 +244,9 @@ class _RegisterPageState extends State<RegisterPage> {
                     FadeInUp(
                       duration: Duration(milliseconds: 2000),
                       child: InkWell(
-                        //onTap: () => Get.to(RegisterPage()),
+                        onTap: () => Get.to(LoginPage()),
                         child: Text(
-                          "Tap here if you don't have an account?",
+                          "Back to the Login Page",
                           style: TextStyle(
                               color: Color.fromRGBO(143, 148, 251, 1)),
                         ),
