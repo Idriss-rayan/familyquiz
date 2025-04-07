7705f39b11b920a07c9ae6b8737b12f2f885f3da
Idriss-rayan <rayanidriss27@gmail.com>
2025-04-07 20:00:17 +0600

    grid view add

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'African Qui',
+      title: 'African Quiz',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: AuthGate(),
