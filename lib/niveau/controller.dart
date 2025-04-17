a7a1589b9f40947ba2e6da98c6a5519f95065411
Idriss-rayan <rayanidriss27@gmail.com>
2025-04-17 22:17:59 +0600

    add controller files for each level

---
+++ lib/niveau/controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Controller extends StatelessWidget {
+  const Controller({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      color: Colors.amber,
+    ));
+  }
+}
