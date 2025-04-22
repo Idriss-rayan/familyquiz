e358f1f5812c99752e296eaf27fee82d9cd22fae
Idriss-rayan <rayanidriss27@gmail.com>
2025-04-22 22:25:22 +0600

    add

--- lib/niveau/niveau1/liste_niveau.dart
+++ lib/niveau/niveau1/liste_niveau.dart
@@ -29,7 +29,6 @@ class ListeNiveau extends StatelessWidget {
               Niveau1Card(),
               Niveau1Card(),
               Niveau1Card(),
-              Niveau1Card(),
             ],
           )),
     );
