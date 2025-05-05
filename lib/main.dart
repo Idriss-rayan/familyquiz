be019efca2662cfdc32fbf8cb9a3ed7650efb797
Idriss-rayan <rayanidriss27@gmail.com>
2025-05-05 15:47:27 +0600

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'welcome/start_screen.dart';
@@ -22,7 +23,11 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'African Quiz',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          ThemeData.dark().textTheme,
+        ),
+      ),
       home: StartScreen(),
     );
   }
